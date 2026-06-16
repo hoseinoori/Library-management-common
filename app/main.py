650739d97e76d3dc8e609e94d73604594f313d20
hoseinoori <hosein86noori@gmail.com>
2026-06-16 18:51:55 +0330

    Setting up the entire website structure and front-end page (HTML/CSS)

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from app.core.config import settings
+from app.database.session import engine, Base
+from app.api.v1.api import api_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title=settings.PROJECT_NAME)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+app.include_router(api_router, prefix="/api/v1")
+
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
