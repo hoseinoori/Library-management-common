a193968d461848b946b0338a0ce5cf480b81ce56
hoseinoori <hosein86noori@gmail.com>
2026-06-16 18:06:00 +0330

    Setting environment variables

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    PROJECT_NAME: str = os.getenv("PROJECT_NAME", "Library System")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./library.db")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "secret")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+
+settings = Settings()
