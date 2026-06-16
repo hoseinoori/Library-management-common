c221599bb61400a43a175a93166f7c6733976ae8
hoseinoori <hosein86noori@gmail.com>
2026-06-16 19:26:59 +0330

    debug-3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
 from app.core.config import settings
 from app.database.session import engine, Base
 from app.api.v1.api import api_router
@@ -11,13 +11,9 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
-
-templates.env.cache = None
-
 app.include_router(api_router, prefix="/api/v1")
 
 
 @app.get("/")
-def read_root(request: Request):
-    return templates.TemplateResponse(name="index.html", context={"request": request})
+def read_root():
+    return FileResponse("templates/index.html")
